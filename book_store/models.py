554c4469cf07bf23f7bab0ef94f40f6052e7ea76
Sajal <sajaldebnath02@gmail.com>
2022-05-28 00:47:12 +0600

    email field added in Author

--- book_store/models.py
+++ book_store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    email = models.CharField(max_length=40, default='abc@gmail.com')
 
     def __str__(self):
         return self.name
